025a24b78e991d573fcfbbc1361dcc1866a2fad9
geniushub <prajapatig2411@gmail.com>
2022-03-29 11:24:39 +0530

    reactfood

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,18 @@
 /* eslint-disable default-case */
 /* eslint-disable no-undef */
 import React, { useEffect, useState } from 'react'
-import { Grid, Paper, TextField } from '@material-ui/core'
-import { Button ,Figure} from 'react-bootstrap'
+import { Grid, Paper, TextField,FormControl ,InputLabel,Input,InputAdornment,IconButton} from '@material-ui/core'
+import { Button, Figure } from 'react-bootstrap'
 import { useParams, useHistory, Link } from "react-router-dom"
+import clsx from 'clsx';
 import axios from 'axios'
 import { omit } from 'lodash'
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { makeStyles } from '@material-ui/core/styles';
 
+ 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -20,7 +25,7 @@ const Login = () => {
         localStorage.removeItem("token");
     }, [])
     const postData = (e) => {
-           e.preventDefault()
+        e.preventDefault()
         let item = {
             // username: values.username,
             email: values.email,
@@ -30,16 +35,22 @@ const Login = () => {
         console.log(item)
 
         axios.post("https://unlimitedfood.herokuapp.com/login", item).then((res) => {
-            if(res.data.success=== true) {
+            if (res.data.success === true) {
                 localStorage.setItem('token', res.data.token);
                 window.location.reload(true)
-            //     history.push('/Tabl')
+                //     history.push('/Tabl')
             }
             // console.log("updare", res)
         })
     }
 
-
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+      };
+    
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
     const validate = (event, name, value) => {
         //A function to validate each input values
 
@@ -94,25 +105,46 @@ const Login = () => {
     }
     const paperStyle = { padding: '30px 20px', width: 300, margin: '20px auto' }
     const marginTop = { marginTop: 5 }
+    
 
 
     return (
         <div>
-           
+
             <Grid>
                 <Paper elevation={20} style={paperStyle}>
                     <Grid align='center'>
-                        <h2> Login <VpnKeyIcon/></h2>
+                        <h2> Login <VpnKeyIcon /></h2>
                     </Grid>
                     <form>
                         {/* <TextField name='username' fullWidth label='UserName' value={values.username} onChange={handleChange} error={Boolean(errors.username)} helperText={errors.username} /> */}
                         <TextField name='email' fullWidth label='Email' value={values.email} onChange={handleChange} error={Boolean(errors.email)} helperText={errors.email} />
-                        <TextField name='password' fullWidth label='Passwrord' value={values.password} onChange={handleChange} error={Boolean(errors.password)} helperText={errors.password} />
-                        <br />
-                        <br />
-                        <Grid align='center'>
-                            <Button type='submit' class='btn btn-info' onClick={ postData}><Link to='/Table'>Login </Link></Button>
-                            {/* <Stack spacing={2} sx={{ width: '100%' }}>
+                        {/* <TextField name='password' fullWidth label='Passwrord' value={values.password} onChange={handleChange} error={Boolean(errors.password)} helperText={errors.password} /> */}
+                    <FormControl >
+                        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                        <Input
+                            id="standard-adornment-password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                    <br />
+                    <br />
+                    <Grid align='center'>
+                        <Button type='submit' class='btn btn-info' onClick={postData}><Link to='/Table'>Login </Link></Button>
+                        {/* <Stack spacing={2} sx={{ width: '100%' }}>
                                 <Button variant="outlined" onClick={handleClick}>
                                  
                                    Submit
@@ -123,19 +155,19 @@ const Login = () => {
                                     </Alert>
                                 </Snackbar>
                             </Stack> */}
-                        </Grid>
+                    </Grid>
+                    <br />
+                    <Grid>
+                        <Link to='/Register'> New Register   </Link>
                         <br />
-                        <Grid>
-                            <Link to='/Register'> New Register   </Link>
-                            <br />
-                            <br />
-                            <Link to='/forget'>Forgate Password</Link>
-                        </Grid>
-                    </form>
-                </Paper>
-                
-            </Grid>
-        </div>
+                        <br />
+                        <Link to='/forget'>Forgate Password</Link>
+                    </Grid>
+                </form>
+            </Paper>
+
+        </Grid>
+        </div >
     )
 }
 
